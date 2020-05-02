3e59e33f71e442b047175b7668cc7ac97ad007e8
keendawg <caelanweaver@live.co.uk>
2020-05-02 15:35:08 +0100

    Front end setup, need backend implementation

---
+++ frontend/src/app/register/register.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  registerForm:FormGroup = new FormGroup({
+    email:new FormControl(null,[Validators.email, Validators.required]),
+    username:new FormControl(null, Validators.required),
+    password:new FormControl(null, Validators.required),
+    cpass:new FormControl(null, Validators.required),
+  })
+
+
+  constructor(private _router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+
+  moveToLogin(){
+    this._router.navigate(['/login']);
+  }
+
+  register(){
+    if(!this.registerForm.valid || (this.registerForm.controls.password.value != this.registerForm.controls.cpass.value)){
+    console.log('Invalid form');
+    return;
+    }
+    console.log(JSON.stringify(this.registerForm.value));
+    
+  }
+}
